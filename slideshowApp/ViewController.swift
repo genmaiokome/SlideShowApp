c667add8a2f1c8f31661c7723f19d91e5a278768
ryosuke.watanabe <genmaiokomey@yahoo.co.jp>
2020-10-05 11:11:56 +0900

    Initial Commit

---
+++ slideshowApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  slideshowApp
+//
+//  Created by 渡辺涼介 on 2020/10/05.
+//  Copyright © 2020 ryosuke.watanabe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
